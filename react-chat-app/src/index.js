b69b32c7ba1c0b8774898535c21c820ee40f0c70
spanki22052 <useful22052@gmail.com>
2020-06-18 13:33:49 +0300

    imported firebase
    imported firebase after creating firebase api

--- react-chat-app/src/index.js
+++ react-chat-app/src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB7m6cqCTLtVpulRAIMW1DYG_OoI_Gu_oM",
+  authDomain: "react-chat-app-1d622.firebaseapp.com",
+  databaseURL: "https://react-chat-app-1d622.firebaseio.com",
+  projectId: "react-chat-app-1d622",
+  storageBucket: "react-chat-app-1d622.appspot.com",
+  messagingSenderId: "555189825204",
+  appId: "1:555189825204:web:b2b527c7ede4e20f2271e1",
+  measurementId: "G-KFCM6E9959",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Hello world</h1>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
