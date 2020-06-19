d0d265ffa0ac12aa7668ea957c0d24f6faa9a6c3
spanki22052 <useful22052@gmail.com>
2020-06-19 19:25:27 +0300

    add working chat viewer

--- react-chat-app/src/chatView/chatView.js
+++ react-chat-app/src/chatView/chatView.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { withStyles } from '@material-ui/core';
-import styles from './styles';
+import { withStyles } from "@material-ui/core";
+import styles from "./styles";
 
 class ChatViewComponent extends Component {
-    render() {
+  render() {
+    const { classes, chat, user } = this.props;
 
-        const { classes } = this.props;
-
-        return (
-            <div className={classes.content}>
-                Hello from chat view component
-            </div>
-        )
-
+    if (chat === undefined) {
+      return <main className={classes.content}></main>;
+    } else {
+      return (
+        <div>
+          <div></div>
+          <main className={classes.content}>
+            {chat.messages.map((_msg, _index) => {
+              return (
+                <div
+                  key={_index}
+                  className={
+                    _msg.sender === user ? classes.userSent : classes.friendSent
+                  }
+                >
+                  {_msg.message}
+                </div>
+              );
+            })}
+          </main>
+        </div>
+      );
     }
+
+    return (
+      <div className={classes.content}>Hello from chat view component</div>
+    );
+  }
 }
 
 export default withStyles(styles)(ChatViewComponent);
