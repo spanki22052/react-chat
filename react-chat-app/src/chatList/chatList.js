2f06c174fff063235c38db0c8241a1505c146903
spanki22052 <useful22052@gmail.com>
2020-06-19 16:33:31 +0300

    solved empty mail problem

--- react-chat-app/src/chatList/chatList.js
+++ react-chat-app/src/chatList/chatList.js
@@ -18,61 +18,79 @@ class ChatListComponent extends Component {
   render() {
     const { classes } = this.props;
 
-    return (
-      <main className={classes.root}>
-        <Button
-          variant="contained"
-          fullWidth
-          color="primary"
-          className={classes.newChatBtn}
-          onClick={this.newChat}
-        ></Button>
-        <List>
-          {this.props.chats.map((_chat, _index) => {
-            return (
-              <div key={_index}>
-                <ListItem
-                  onClick={() => this.selectChat(_index)}
-                  className={classes.listItem}
-                  selected={this.props.selectedChatIndex === _index}
-                  alignItems="flex-start"
-                >
-                  <ListItemAvatar>
-                    <Avatar alt="Remy Sharp">
-                      {
+    if (this.props.chats.length > 0) {
+      return(
+        <main className={classes.root}>
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            className={classes.newChatBtn}
+            onClick={this.newChat}
+          >NEW MESSAGE</Button>
+          <List>
+            {this.props.chats.map((_chat, _index) => {
+              return (
+                <div key={_index}>
+                  <ListItem
+                    onClick={() => this.selectChat(_index)}
+                    className={classes.listItem}
+                    selected={this.props.selectedChatIndex === _index}
+                    alignItems="flex-start"
+                  >
+                    <ListItemAvatar>
+                      <Avatar alt="Remy Sharp">
+                        {
+                          _chat.users
+                            .filter(
+                              (_user) => _user !== this.props.userEmail
+                            )[0]
+                            .split("")[0]
+                        }
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={
                         _chat.users
                           .filter((_user) => _user !== this.props.userEmail)[0]
                           .split("")[0]
                       }
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={
-                      _chat.users
-                        .filter((_user) => _user !== this.props.userEmail)[0]
-                        .split("")[0]
-                    }
-                    secondary={
-                      <React.Fragment>
-                        <Typography component="span" color="textPrimary">
-                          {" "}
-                          {_chat.messages[
-                            _chat.messages.length - 1
-                          ].message.substring(0, 30)}
-                        </Typography>
-                      </React.Fragment>
-                    }
-                  ></ListItemText>
-                </ListItem>
-                <Divider></Divider>
-              </div>
-            );
-          })}
-        </List>
-      </main>
-    );
+                      secondary={
+                        <React.Fragment>
+                          <Typography component="span" color="textPrimary">
+                            {" "}
+                            {_chat.messages[
+                              _chat.messages.length - 1
+                            ].message.substring(0, 30)}
+                          </Typography>
+                        </React.Fragment>
+                      }
+                    ></ListItemText>
+                  </ListItem>
+                  <Divider></Divider>
+                </div>
+              );
+            })}
+          </List>
+        </main>
+      );
+    } else {
+      return (
+        <main className={classes.root}>
+          <Button
+            variant="contained"
+            fullWidth
+            color="primary"
+            onClick={this.newChat}
+            className={classes.newChatBtn}
+          >
+            New message
+          </Button>
+          <List></List>
+        </main>
+      );
+    }
   }
-
   newChat = () => {
     console.log(" new chat click");
   };
